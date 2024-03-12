cfe650e9b69ee965164906ea7e98e8d5197d1460
hasnaerguig1999 <hasnaerguig1999@gmail.com>
2024-03-12 15:46:21 +0000

    HSTOR-9 Valider les données entrantes et sortantes

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { getAllCategories,getCategory,createCategory,updateCategory,deleteCategory } from '../controllers/categoryController';
 
-
+// import authMiddleware from '../middlewares/authMiddleware';
 
 const router = Router();
 
 
 
 router.get('/', getAllCategories);
-
+// router.use(authMiddleware);
 router.get('/:id', getCategory);
 router.post('/', createCategory);
 router.put('/:id', updateCategory);
