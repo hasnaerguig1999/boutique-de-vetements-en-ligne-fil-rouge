e5854349be3483c2e804d4c8eb052bb2d630c371
hasnaerguig1999 <hasnaerguig1999@gmail.com>
2024-03-12 15:45:06 +0000

    HSTOR-8 Implémenter les API CRUD pour les catégories

---
+++ backend/routes/category.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { getAllCategories,getCategory,createCategory,updateCategory,deleteCategory } from '../controllers/categoryController';
+
+
+
+const router = Router();
+
+
+
+router.get('/', getAllCategories);
+
+router.get('/:id', getCategory);
+router.post('/', createCategory);
+router.put('/:id', updateCategory);
+router.delete('/:id',deleteCategory);
+
+export default router;
